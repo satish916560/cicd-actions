5915a7879ecea4999283af6fe1d6edbed91fd0c9
satish <703371783@genpact.com>
2024-01-25 16:46:38 +0530

    t

---
+++ .github/workflows/Libraries/python/dabdemo/setup.py
@@ -0,0 +1,16 @@
+# Filename: setup.py
+from setuptools import setup, find_packages
+
+import dabdemo
+
+setup(
+  name = "dabdemo",
+  version = dabdemo.__version__,
+  author = dabdemo.__author__,
+  url = "https://dbc-25222c34-9dd6.cloud.databricks.com",
+  author_email = "satish456.cse@gmail.com",
+  description = "<my-package-description>",
+  packages = find_packages(include = ["dabdemo"]),
+  entry_points={"group_1": "run=dabdemo.__main__:main"},
+  install_requires = ["setuptools"]
+)
